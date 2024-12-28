b5681699dc686bade1b88fb7a4d67023181e64a9
KDamache <kamildamache@gmail.com>
2024-12-28 18:41:21 +0100

    FEA-10 : Set up API communication
    Related to #10 .

---
+++ AppLidra.Client/Handlers/HttpClientAuthorizationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using AppLidra.Client.Services;
+namespace AppLidra.Client.Handlers;
+public static class HttpClientAuthorizationExtensions
+{
+    public static void AddAuthorizationHandler(this IServiceCollection services)
+    {
+        services.AddScoped<AuthorizationMessageHandler>();
+        services.AddScoped(sp =>
+        {
+            var handler = sp.GetRequiredService<AuthorizationMessageHandler>();
+            handler.InnerHandler = new HttpClientHandler();
+
+            var client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri("https://localhost:44354/")
+            };
+
+            return client;
+        });
+    }
+}
--- AppLidra.Client/Program.cs
+++ AppLidra.Client/Program.cs
@@ -1,4 +1,7 @@
 using AppLidra.Client;
+using AppLidra.Client.Services;
+using AppLidra.Client.Handlers;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +9,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationHandler();
+builder.Services.AddBlazoredLocalStorage();
+
+
 
 await builder.Build().RunAsync();
