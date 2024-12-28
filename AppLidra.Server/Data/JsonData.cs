3a9be19e0b6eaee44f3492c66e92e9c327698e35
KDamache <kamildamache@gmail.com>
2024-12-28 17:42:48 +0100

    FEA-8 : Database Setup
    Various modifications in program.cs and the app settings.
    Creation of two cs classes and a js file for Database management.
    Related to #8 .

---
+++ AppLidra.Server/Data/JsonData.cs
@@ -0,0 +1,10 @@
+﻿using AppLidra.Shared.Models;
+
+namespace AppLidra.Shared.Services;
+
+public class JsonData
+{
+    public List<Project> Projects { get; set; } = [];
+    public List<User> Users { get; set; } = [];
+    public List<Expense> Expenses { get; set; } = [];
+}
