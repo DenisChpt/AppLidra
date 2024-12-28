c716994c8cd3b9bb874e46dc24289eca649a6a7b
KDamache <kamildamache@gmail.com>
2024-12-28 17:49:54 +0100

    FEA-4 Update AuthController
    Added all the necessary routes to the authentication controller.
    
    Related to #4 .

--- AppLidra.Server/Controllers/AuthController.cs
+++ AppLidra.Server/Controllers/AuthController.cs
@@ -1,40 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppLidra.Server.Data;
 using AppLidra.Shared.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
+namespace AppLidra.Server.Controllers;
+
 [ApiController]
 [Route("api/[controller]")]
-public class AuthController : ControllerBase
+public class AuthController(JsonDataStore store, IConfiguration configuration) : ControllerBase
 {
+    private readonly JsonDataStore _store = store;
+    private readonly IConfiguration _configuration = configuration;
+
+
     [HttpPost("login")]
-    public IActionResult Login(User user)
+    public IActionResult Login([FromBody] LoginModel logs)
     {
-        // Verufy the id's (auth logic)
-        return Ok();
+        var existingUser = _store.Users.FirstOrDefault(u => u.Email == logs.Email && u.Password == logs.Password);
+        if (existingUser == null)
+        {
+            return Unauthorized("Invalid credentials");
+        }
+
+        var token = GenerateJwtToken(existingUser);
+        return Ok(new { Token = token });
     }
 
     [HttpPost("register")]
-    public IActionResult Register(User user)
+    public IActionResult Register([FromBody] User user)
     {
-        // Add user in the database
+        if (_store.Users.Any(u => u.Email == user.Email))
+        {
+            return BadRequest("User already exists.");
+        }
+        else if (_store.Users.Any(u => u.UserName == user.UserName))
+        {
+            return BadRequest("User Name already used.");
+        }
+
+        user.Id = _store.Users.Count != 0 ? _store.Users.Max(u => u.Id) + 1 : 1;
+        _store.Users.Add(user);
+        _store.SaveChanges();
+
         return Ok();
+    }
+
+    [HttpGet("all-users")]
+    [AllowAnonymous]
+    public IActionResult GetAllUsers()
+    {
+        return Ok(_store.Users);
     }
 
     private string GenerateJwtToken(User user)
     {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes("YourSecretKeyHere");
-        var tokenDescriptor = new SecurityTokenDescriptor
+        JwtSecurityTokenHandler tokenHandler = new();
+        string? temp = _configuration["Jwt:Secret"] ?? throw new Exception("Jwt:Secret not found in configuration.");
+        byte[] key = Encoding.UTF8.GetBytes(temp);
+        SecurityTokenDescriptor tokenDescriptor = new()
         {
-            Subject = new ClaimsIdentity(new[] { new Claim("id", user.Id.ToString()) }),
+            Subject = new ClaimsIdentity(
+            [
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new Claim(ClaimTypes.Email, user.Email)
+        ]),
             Expires = DateTime.UtcNow.AddDays(7),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256)
         };
         var token = tokenHandler.CreateToken(tokenDescriptor);
         return tokenHandler.WriteToken(token);
     }
-
 }
